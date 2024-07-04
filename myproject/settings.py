eaf12dcd78dece7794954509b3b6f5106f5b6a8b
Sydni <sydnidlevy@gmail.com>
2024-07-04 09:06:42 -0400

    achat works with daphne and using computer wifi ip address. also fixed not found style.css error. this required downloading a bunch of stuff, so i need to compile the instructions

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from django.core.asgi import get_asgi_application
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'myproject.urls'
 
@@ -124,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
-    BASE_DIR / "myapp/static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
